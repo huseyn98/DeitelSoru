753d7c4941e03c26719a3e4ae1993bad90033d6c
Huseyn <huseynvaliyev2015@gmail.com>
2017-04-01 01:17:16 +0300

    code

--- kredi.c
+++ kredi.c
@@ -5,26 +5,7 @@ int main(){
   int hesap_num;
       printf("Hesap numarasını girin (bitirmek için -1): " );
       scanf("%d",&hesap_num);
-      printf("Başalangıç balansı giriniz: ");
-      scanf("%f",&bas_balans);
-      printf("Toplam harcamayı girin: ");
-      scanf("%f",&harcama);
-      printf("Toplam yatırlan parayı girin: ");
-      scanf("%f",&yatirim);
-      printf("Kredi limitini girin: ");
-      scanf("%f",&limit);
-      balans = 0;
-
-      balans = (bas_balans+harcama-yatirim);
-  if(balans>limit){
-    printf("Hesap:\t%d\n",hesap_num);
-    printf("Kredi Limiti:%f\n",limit);
-    printf("Balans:\t%f\n",balans);
-    printf("Kredi limiti aşımı.\n");
-  }
   while (hesap_num != (-1)) {
-    printf("Hesap numarasını girin (bitirmek için -1): " );
-    scanf("%d",&hesap_num);
     printf("Başalangıç balansı giriniz: ");
     scanf("%f",&bas_balans);
     printf("Toplam harcamayı girin: ");
@@ -41,6 +22,8 @@ int main(){
   printf("Kredi Limiti:%f\n",limit);
   printf("Balans:\t%f\n",balans);
   printf("Kredi limiti aşımı.\n");
+  printf("Hesap numarasını girin (bitirmek için -1): " );
+  scanf("%d",&hesap_num);
   }
 
 
