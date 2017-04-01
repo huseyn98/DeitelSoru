333ff1561e42f6168b93ef5dd00bf77cede58f93
Huseyn <huseynvaliyev2015@gmail.com>
2017-04-01 16:07:54 +0300

    code

---
+++ faktoriyel.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+
+int main(){
+  int n,faktoriyel;
+  faktoriyel=1;
+  printf("n i girin: s");
+  scanf("%d",&n);
+  while(n>=1){
+    faktoriyel=faktoriyel*n;
+    n=n-1;
+  }
+  printf("%d\n",faktoriyel);
+return 0;
+}
